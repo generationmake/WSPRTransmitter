b049256e68836514c56ca7b9aac25ac714e0b4c7
Bernhard Mayer <bernhard@generationmake.de>
2021-05-07 08:53:13 +0200

    disabled display backlight to save battery power

--- WSPRTransmitter.ino
+++ WSPRTransmitter.ino
@@ -53,7 +53,7 @@ void encode()
 
   // Reset the tone to the base frequency and turn on the output
   si5351.output_enable(SI5351_CLK0, 1);
-  digitalWrite(LED_BUILTIN, HIGH);
+//  digitalWrite(LED_BUILTIN, HIGH);
 
   for(i = 0; i < symbol_count; i++)
   {
@@ -64,7 +64,7 @@ void encode()
 
   // Turn off the output
   si5351.output_enable(SI5351_CLK0, 0);
-  digitalWrite(LED_BUILTIN, LOW);
+//  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void set_tx_buffer()
